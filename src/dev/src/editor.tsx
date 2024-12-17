47e0d1de2d5528fd975691ece951efc6e4fe293e
dimicuss <dimicuss@gmail.com>
2024-12-17 16:57:49 +0300

    feat(frontend): minor improvements

--- src/dev/src/editor.tsx
+++ src/dev/src/editor.tsx
@@ -5,6 +5,7 @@ import {schema} from "prosemirror-schema-basic"
 import {addListNodes} from "prosemirror-schema-list"
 import {exampleSetup} from "prosemirror-example-setup"
 import {useEffect, useRef} from "react"
+import styled from "styled-components"
 
 export const Editor = () => {
   const ref = useRef<HTMLDivElement>(null)
@@ -28,30 +29,50 @@ export const Editor = () => {
 
 
   return (
-    <>
-      <div ref={ref}></div>
-      <div ref={initialRef} style={{display: 'none'}}>
-        <h3>
-          Hello ProseMirror
-        </h3>
-        <p>
-          This is editable text. You can focus it and start typing.</p>
-        <p>
-          To apply styling, you can select a piece of text and manipulate
-          its styling from the menu. The basic schema
-          supports <em>emphasis</em>, <strong>strong text</strong>,
-          <a href="http://marijnhaverbeke.nl/blog">links</a>, <code>code font</code>.
-        </p>
-        <p>
-          Block-level structure can be manipulated with key bindings (try
-          ctrl-shift-2 to create a level 2 heading, or enter in an empty
-          textblock to exit the parent block), or through the menu.
-        </p>
-        <p>
-          Try using the “list” item in the menu to wrap this paragraph in
-          a numbered list.
-        </p>
-      </div>
-    </>
+    <Container>
+      <Left>
+        <div ref={ref}></div>
+        <div ref={initialRef} style={{display: 'none'}}>
+          <h3>
+            Hello ProseMirror
+          </h3>
+          <p>
+            This is editable text. You can focus it and start typing.</p>
+          <p>
+            To apply styling, you can select a piece of text and manipulate
+            its styling from the menu. The basic schema
+            supports <em>emphasis</em>, <strong>strong text</strong>,
+            <a href="http://marijnhaverbeke.nl/blog">links</a>, <code>code font</code>.
+          </p>
+          <p>
+            Block-level structure can be manipulated with key bindings (try
+            ctrl-shift-2 to create a level 2 heading, or enter in an empty
+            textblock to exit the parent block), or through the menu.
+          </p>
+          <p>
+            Try using the “list” item in the menu to wrap this paragraph in
+            a numbered list.
+          </p>
+        </div>
+      </Left>
+      <Right>
+
+      </Right>
+    </Container>
   )
 }
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template: 'l r' / 1fr 1fr;
+`
+
+const Left = styled.div`
+  grid-area: l;
+`
+
+const Right = styled.div`
+  grid-area: r;
+`
+
